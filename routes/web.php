06b4c0bad809b130c41b9ca8a700da0d6788e850
Camilo Cabuya Berrio <cabuyaberriojin@gmail.com>
2024-09-18 17:29:43 -0500

    google-login

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\GoogleLoginController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('login');
+});
+
+Route::get('/google/redirect', [GoogleLoginController::class, 'redirectToGoogle'])->name('google.redirect');
+Route::get('/google/callback', [GoogleLoginController::class, 'handleGoogleCallback'])->name('google.callback');
